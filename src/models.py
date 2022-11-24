9b82561f999ac63749c0e97486d06e0e149d64ed
LOGEDI <nelsonlogedi@gmail.com>
2022-11-24 12:20:31 +0000

    my histagram.com first initial

--- src/models.py
+++ src/models.py
@@ -8,23 +8,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email= Column(String(250),nullable=False)
+    password = Column(String(250),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Followers(Base):
+    __tablename__ = 'followers'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250))
+    email = Column(String(250),ForeignKey('email.id'))
+    password = Column(String(250), nullable=False)
+    #person_id = Column(Integer, ForeignKey('person.id'))
+    #person = relationship(Person)
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True) 
+    author_id = Column(String(250))
+    follower_id =Column(String(250))
+    
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True) 
+    author_id = Column (String(250))
+    follower_id =Column(String(250))
+   # video = Column(String(250))
+    #image = Column(String(250))
+    #music = Column (String(250))
+    
+
 
     def to_dict(self):
         return {}
